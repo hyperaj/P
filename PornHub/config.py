3fb58a196d48c8bbda9d9e5fe5efbb25b53ce0f8
hyperaj <168776874+hyperaj@users.noreply.github.com>
2024-05-09 14:22:21 +0530

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -1,12 +1,12 @@
 from typing import List
 
-API_ID: int = insert_your_own_api_id_here
-API_HASH: str = "insert_your_own_api_hash_here"
-TOKEN: str = "insert_your_bot_token_here"
+API_ID: int = "23255264"
+API_HASH: str = "df813db94fa514e09e189044274f470b"
+TOKEN: str = "6486584540:AAFYLW1mfCMcSwUb8OwWsDvQIbBaCAFjqcU"
 
-log_chat: int = -1001662591986
-sub_chat: str = "SakuraEmpireTeam"
-sudoers: List[int] = [2137482758, 1249591948]
+log_chat: int = "-1002038504480"
+sub_chat: str = "ajajasssk"
+sudoers: List[int] = ["6900132473"]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
 
 # notes
